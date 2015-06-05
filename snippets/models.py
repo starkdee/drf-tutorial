e7bc93e5aafc98bb634191379a5ef49644b1692c
Dmitry <dm.chernushov@gmail.com>
2015-06-05 14:48:41 +0300

    Added highlight field

--- snippets/models.py
+++ snippets/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
+from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
+from pygments import highlight
+from pygments.formatters.html import HtmlFormatter
+
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
@@ -8,12 +11,23 @@ STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 
 class Snippet(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='snippets')
+
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
     lineos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
-
+    highlighted = models.TextField()
+    
     class Meta:
         ordering = ('created',)
+
+    def save(self, *args, **kwargs):
+        lexer = get_lexer_by_name(self.language)
+        lineos = self.lineos and 'table' or False
+        options = self.title and {'title': self.title} or {}
+        formatter = HtmlFormatter(style=self.style, lineos=lineos, full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
